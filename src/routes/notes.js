31429dc028503ef77be77740f27f4a0e57501ede
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-04-24 17:00:41 -0300

    Sending edit actions | Feat

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -4,6 +4,18 @@ const { ObjectId} = require('mongodb')
 
 const router  = Router()
 
+// view de detalhes
+router.get('/:id', async function(req, res){
+
+    const id = new ObjectId(req.params.id)
+
+    const note = await db.getDb().db().collection('notes').findOne({ _id: id })
+
+    res.render('notes/detail', { note })
+
+})
+
+//  criação de rota
 router.get('/', function(req, res){
 
     res.render('notes/create')
@@ -21,6 +33,35 @@ router.post('/', function(req, res){
         .collection('notes')
         .insertOne({ title: title, description: description})
     res.redirect(301, '/')
+})
+
+// view de edição de notas
+
+router.get('/edit/:id', async function(req, res){
+
+    const id = new ObjectId(req.params.id)
+    const note = await db.getDb().db().collection('notes').findOne({ _id: id})
+
+    res.render('notes/edit', {note})
+
+})
+
+//edição de notas
+router.post('/update',function(req, res){
+
+    const data = req.body
+    const id = new ObjectId(data.id)
+    const title = data.title
+    const description = data.description
+
+    db.getDb()
+        .db()
+        .collection('notes')
+        .updateOne({_id: id}, {$set: {title: title, description: description }})
+
+
+    res.redirect(301, '/')
+
 })
 
 //remoção da tarefa
