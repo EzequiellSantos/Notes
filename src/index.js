7a48169b9ca0bff747f530d85adca5795d3b306a
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-12-24 10:37:22 -0300

    Update Fixes

--- src/index.js
+++ src/index.js
@@ -10,32 +10,13 @@ const port = 8000
 // DB 
 const db = require('./db/connection')
 
-//init db
-db.initDb((error, db) => {
-
-    if(error){
-        console.log(error);
-        console.log('^^^^^ error na incialização ^^^^')
-        // Se houver um erro na inicialização do banco de dados, encerre o processo
-        process.exit(1);
-    }else{
-
-        console.log('Banco conectado :)');
-        app.listen(port, () => {
-            console.log(`Projeto rodadndo na porta ${port}`);
-        })
-
-    }
-
-})
-
 // template engine
-app.engine('handlebars', exphbs.engine({
-    defaultLayout:'main'
-}))
+app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('/public'))
 app.use(bodyparser.urlencoded({ extended: true }))
+
+
 
 // Importação de rotas
 const notesRoutes = require('./routes/notes')
@@ -51,4 +32,17 @@ app.get('/', async function(req, res) {
 
 app.use('/notes', notesRoutes)
 
-module.exports = app;
+db.initDb((error, db) => {
+
+    if(error){
+        console.log(error);
+    }else{
+
+        console.log('Banco conectado :)');
+        app.listen(port, () => {
+            console.log(`Projeto rodadndo na porta ${port}`);
+        })
+
+    }
+
+})
