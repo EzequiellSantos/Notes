9b04d43f4b1626f26f63bc0e3cf083ef418af5cf
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-12-25 12:25:11 -0300

    Update for Vercel Deploy

--- src/api/index.js
+++ src/api/index.js
@@ -2,24 +2,24 @@
 require('dotenv').config()
 const express = require('express');
 const exphbs = require ('express-handlebars');
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
+const path = require("path");
 
 const app = express()
 const port = 8000
 
 // DB 
-const db = require('./db/connection')
+const db = require('../db/connection')
 
 // template engine
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
-app.use(express.static('/public'))
+app.use(express.static("public"))
 app.use(bodyparser.urlencoded({ extended: true }))
 
 
-
 // Importação de rotas
-const notesRoutes = require('./routes/notes')
+const notesRoutes = require('../routes/notes')
 
 // rotas
 app.get('/', async function(req, res) {
@@ -46,3 +46,5 @@ db.initDb((error, db) => {
     }
 
 })
+
+module.exports = app
