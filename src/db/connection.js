fafa7512de6e3b8408c6fbf0f87c49e77b2b856d
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2025-02-10 17:46:31 -0300

    test conection for deploy | Test #1

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,4 @@
 const { MongoClient } = require("mongodb");
-const app = require('../db/connection');  // Não sei se essa linha é necessária no seu caso, mas mantive.
 require("dotenv").config();
 
 const url = process.env.MONGODB_URI;
@@ -8,20 +7,20 @@ const url = process.env.MONGODB_URI;
 let _db;
 
 // Função para conectar ao banco de dados
-const initDb = async (cb) => {
+const initDb = async () => {
   if (_db) {
     console.log("Banco de dados já inicializado.");
-    return cb(null, _db);
+    return _db;
   }
 
   try {
     const client = await MongoClient.connect(url, { useUnifiedTopology: true });
     _db = client.db("notesDb"); // Especifica o banco de dados
     console.log("Conexão com o banco de dados estabelecida.");
-    cb(null, _db);
+    return _db;
   } catch (error) {
     console.error("Erro ao conectar ao banco de dados:", error);
-    cb(error);
+    throw error; // Lança o erro para ser tratado no chamador
   }
 };
 
@@ -32,28 +31,6 @@ const getDb = () => {
   }
   return _db;
 };
-
-// Função para reiniciar a conexão com o MongoDB
-const restartDbConnection = async () => {
-  if (_db) {
-    // Fechar a conexão atual
-    const client = _db.client;
-    await client.close();
-    console.log("Conexão com o banco de dados fechada.");
-  }
-
-  // Re-estabelecer a conexão com o banco
-  initDb((err, db) => {
-    if (err) {
-      console.error("Erro ao reiniciar a conexão com o MongoDB:", err);
-    } else {
-      console.log("Conexão com o MongoDB reiniciada com sucesso.");
-    }
-  });
-};
-
-// Intervalo de 10 minutos (600000 ms) para reiniciar a conexão
-setInterval(restartDbConnection, 300000); // 600000 ms = 10 minutos
 
 module.exports = {
   initDb,
